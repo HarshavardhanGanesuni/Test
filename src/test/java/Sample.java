c332ecbf3eadcdc197c9d85d5794a29d0ba60e9d
Harsha <gharshacse@gmail.com>
2020-07-09 22:34:08 +0530

    Initial commit

---
+++ src/test/java/Sample.java
@@ -0,0 +1,11 @@
+public class Sample {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        hello();
+    }
+
+    public static void hello()
+    {
+        System.out.println("Method one");
+    }
+}
